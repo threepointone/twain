049771b05e14f56296f3eb644ee225cc5d8abdde
Sunil Pai <threepointone@gmail.com>
2013-02-05 11:04:58 +0530

    until tests

--- tests/test.js
+++ tests/test.js
@@ -4,23 +4,92 @@ var Twain = require('../twain'),
     __should = require('should');
 
 describe('util', function() {
-    describe('each', function() {
-
-    });
 
     describe('each', function() {
+
+        it('should cycle through an array', function() {
+            var str = '',
+                arr = [1, 2, 3];
+
+            util.each(arr, function(el, i) {
+                str += el;
+            });
+
+            str.should.eql('123');
+        });
+
+        it('should cycle through an object', function() {
+            var str = '',
+                obj = {
+                    a: 1,
+                    b: 2,
+                    c: 3
+                };
+
+            util.each(obj, function(el, key) {
+                str += el;
+            });
+
+            str.should.eql('123');
+        });
 
     });
 
     describe('isValue', function() {
 
+        it('should return false for null and undefined', function() {
+            util.isValue(null).should.eql(false);
+            util.isValue(undefined).should.eql(false);
+        });
+
+        it('should return true for everything else', function() {
+            util.isValue({}).should.eql(true);
+            util.isValue([]).should.eql(true);
+            util.isValue(123).should.eql(true);
+            util.isValue('abc').should.eql(true);
+            util.isValue('').should.eql(true);
+            util.isValue(0).should.eql(true);
+            util.isValue(-1).should.eql(true);
+        });
     });
 
     describe('extend', function() {
-
+        it('should copy properties onto the first argument', function() {
+            var o = {};
+            util.extend(o, {
+                a: 1
+            }, {
+                b: 2
+            });
+            (o.a).should.eql(1);
+            (o.b).should.eql(2);
+        });
     });
 
     describe('collect', function() {
+        it('should cycle through the object, and return an object based on the callback', function() {
+            var sample = function(x) {
+                    return {
+                        gabba: x,
+                        fn: function() {
+                            return x;
+                        }
+                    };
+                };
+            var obj = {
+                a: sample('a'),
+                b: sample('b'),
+                c: sample('c')
+            };
+
+            (util.collect(obj, function(o){
+                return o.fn();
+            })).should.eql({a:'a', b:'b', c:'c'});
+
+            (util.collect(obj, 'gabba')).should.eql({a:'a', b:'b', c:'c'});
+
+        });
+
 
     });
 });

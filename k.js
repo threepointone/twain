54637aa557837810e6cd9de5d2614b2835254ac3
Sunil Pai <threepointone@gmail.com>
2013-02-06 01:59:06 +0530

    error logging when build fails

--- k.js
+++ k.js
@@ -6,8 +6,12 @@ require('k')({
 .compress()
 .write()
 .fn(function(done) {
-    console.log('built dist/twain.min.js:', Math.round((this.files[0].content.length / 1024) * 1000) / 1000 + 'k')
+    console.log('built dist/twain.min.js:', Math.round((this.files[0].content.length / 1024) * 1000) / 1000 + 'k');
+    done();
 })
 .fail(function(err) {
-    console.log('ERROR' + err);
+    console.log('build failed :(')
+    console.log(err.stack);
+    process.exit(1);
+
 });

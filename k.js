065631813c0d212018363398345a7ed150bfbbec
Sunil Pai <threepointone@gmail.com>
2013-02-05 16:22:24 +0530

    kb math

--- k.js
+++ k.js
@@ -6,7 +6,7 @@ require('k')({
 .read()
 .compress().write()
 .fn(function(done){
-    console.log('built dist/twain.min.js:', (this.files[0].content.length/1000) + 'k')    
+    console.log('built dist/twain.min.js:', Math.round((this.files[0].content.length/1024)*1000)/1000 + 'k')    
 })
 .fail(function(err){
     console.log('ERROR' + err);
